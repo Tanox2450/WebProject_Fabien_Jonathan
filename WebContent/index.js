76d812e8e7cedf54b194512fd90a4fe3f720f0a2
Jonathan Pageau <pageau.jon@gmail.com>
2015-07-10 14:24:00 +0200

    Vendredi 14h30 bannière + formulaire

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
 	console.log("JavaScript ready!");
 	$("#feuille").hide();
-	$("#feuille").fadeIn(2000);
+	$("#feuille").fadeIn(1500);
 	$("#identification").click(popUpIdentification);
 	$(".menuBox").mouseover(addClass);
 	$(".menuBox").mouseout(removeClass);
 	$(".fiche").click(goToUrl);
 	$(window).on('resize', placeIdBox);
 	placeIdBox();
+	$("#inscription").click(goToUrl);
 	$("#formulaireConnexion").hide();
-
+	$("#inscription").hide();
+	$("#buttonNext").click(function() {
+		slides(true);
+	});
 });
 
 function popUpIdentification() {
 	console.log("pop - up");
 	$("#userName").focus();
 	$("#formulaireConnexion").toggle("slow");
+	$("#inscription").slideToggle("slow");
 }
 
 function addClass() {
@@ -29,40 +34,51 @@ function removeClass() {
 }
 
 function placeIdBox(event) {
-	var posFeuille = $("#feuille").position().left;
+	var posFeuille = $("#feuille").offset().left;
 	$("#identification").css("right", posFeuille);
 	$("#formulaireConnexion").css("right",
 			posFeuille + $("#identification").width());
+	$("#inscription").css("right", posFeuille);
+	$("#inscription").css("top", $("#identification").height());
 }
 
-function goToUrl(event){
+function goToUrl(event) {
 	window.location = ($(this).attr("data-url"));
+//	console.log(($(this).attr("data-url")));
 }
 
-
-
-//=========================================
-
-
+// =========================================
+var nbImage = 3;
+var count = 0;
 var on = true;
-var myVar = setInterval(function(){ if(on) {slides()}on=true;},3000);
+var myVar = setInterval(function() {
+	if (on) {
+		slides()
+	}
+	on = true;
+}, 3000);
 
 function slides(i) {
-  document.getElementById("divImg2").style.width="0px";
-  document.getElementById("divImg3").style.width="100%";
-  var slide = document.getElementById("divImg1");
-  document.getElementById("slide").appendChild(slide);
-  if(i) {
-   on = false; 
-  }
-}
-function back_slide(i) {
-  document.getElementById("divImg2").style.width="0px";
-  document.getElementById("divImg1").style.width="100%";
-  var slide = document.getElementById("divImg5");
-  var slide2 = document.getElementById("divImg1");
-  document.getElementById("slide").insertBefore(slide,slide2);
-  if(i) {
-   on = false; 
-  }
+
+	if (count >= nbImage) {
+		count = 0;
+	}
+	count++;
+
+//	console.log($("#divImg" + count).width());
+
+//	console.log($("#divImg" + count).attr("z-index"));
+	$("#divImg" + count).width("1300px");
+
+//	console.log($("#divImg" + count).width());
+
+	if (count == 1) {
+		$("#divImg" + (nbImage).toString()).width("0%");
+	} else {
+		$("#divImg" + (count - 1).toString()).width("0%");
+	}
+
+	if (i) {
+		on = false;
+	}
 }
